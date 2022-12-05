f83316a755c3f6db8e3a95ea6e5d8c78844c19b3
Murilo Rodegheri <murilo.rodegheri@dowjones.com>
2022-12-05 00:13:16 -0300

    test: email validator, validator mock

---
+++ tests/utils/email-validator.spec.js
@@ -0,0 +1,36 @@
+const validator = require('validator')
+
+class EmailValidator {
+  isValid (email) {
+    return validator.isEmail(email)
+  }
+}
+
+const makeSut = () => {
+  return new EmailValidator()
+}
+
+describe('Email Validator', () => {
+  test('Should return true if validator returns true', () => {
+    const sut = makeSut()
+    const isEmailValid = sut.isValid('valid_email@mail.com')
+
+    expect(isEmailValid).toBe(true)
+  })
+
+  test('Should return false if validator returns false', () => {
+    validator.isEmailValid = false
+    const sut = makeSut()
+    const isEmailValid = sut.isValid('invalid_email@mail.com')
+
+    expect(isEmailValid).toBe(false)
+  })
+
+  test('Should call validator with correct email', () => {
+    const sut = makeSut()
+    const email = 'any_email@mail.com'
+    sut.isValid(email)
+
+    expect(validator.email).toBe(email)
+  })
+})
