b24ff0592fcd123098b25729822b45d5876e675c
Murilo Rodegheri <murilo.rodegheri@dowjones.com>
2022-12-17 11:49:28 -0300

    test: ensure authUseCase throws if no dependency is provided

--- tests/domain/usecases/auth-usecase.spec.js
+++ tests/domain/usecases/auth-usecase.spec.js
@@ -77,6 +77,13 @@ describe('Auth UseCase', () => {
     expect(loadUserByEmailRepositorySpy.email).toBe('any_email@mail.com')
   })
 
+  test('Should throw if no dependency is provided', async () => {
+    const sut = new AuthUseCase()
+    const promise = sut.auth('any_email@mail.com', 'any_password')
+
+    await expect(promise).rejects.toThrow(new MissingParamError('loadUserByEmailRepository'))
+  })
+
   test('Should throw if no LoadUserByEmailRepository is provided', async () => {
     const sut = new AuthUseCase({})
     const promise = sut.auth('any_email@mail.com', 'any_password')
